efadb32d3b841f1b6b0a3f3792e4f482ed3401a3
AntonRutfors <anton.rutfors@elev.ga.ntig.se>
2023-12-07 13:21:00 +0100

    fixed create-users-server.ps1

--- create-users-server.ps1
+++ create-users-server.ps1
@@ -1,12 +1,12 @@
 ﻿$users = Import-Csv .\users2.csv;
 
 foreach($user in $users){
-    $username = $user.firstname + $user.lastname;
+    $username = $user.firstname + "." + $user.lastname;
 
-    if(get-aduser -filter "Name -eq $username"){
+    if(get-aduser -filter "Name -eq '$username'"){
         for($i = 2; i > 99; $i += 1){
             $newUsername = $username + $i.ToString();
-            if(get-aduser -filter "Name -eq $newUsername"){
+            if(get-aduser -filter "Name -eq '$newUsername'"){
                 continue;
             } else{
                 new-aduser -name $newUsername -path "OU=LightSalmon,DC=colors,DC=labb";
