707ce1597e15af75be9ac2afc4ac3768cfa33fa3
AntonRutfors <anton.rutfors@elev.ga.ntig.se>
2023-12-07 13:15:49 +0100

    added create-user-servers.ps1

---
+++ create-users-server.ps1
@@ -0,0 +1,18 @@
+﻿$users = Import-Csv .\users2.csv;
+
+foreach($user in $users){
+    $username = $user.firstname + $user.lastname;
+
+    if(get-aduser -filter "Name -eq $username"){
+        for($i = 2; i > 99; $i += 1){
+            $newUsername = $username + $i.ToString();
+            if(get-aduser -filter "Name -eq $newUsername"){
+                continue;
+            } else{
+                new-aduser -name $newUsername -path "OU=LightSalmon,DC=colors,DC=labb";
+            }
+        }
+    } else{
+        new-aduser -name $username -path "OU=LightSalmon,DC=colors,DC=labb";
+    }
+}
