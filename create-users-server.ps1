2ef4af14a20ca3222bf77ecd3818c59309891a11
AntonRutfors <anton.rutfors@elev.ga.ntig.se>
2023-12-07 14:06:59 +0100

    simplified add user script

--- create-users-server.ps1
+++ create-users-server.ps1
@@ -3,16 +3,5 @@
 foreach($user in $users){
     $username = $user.firstname + "." + $user.lastname;
 
-    if(get-aduser -filter "Name -eq '$username'"){
-        for($i = 2; $i > 99; $i += 1){
-            $newUsername = $username + $i.ToString();
-            if(get-aduser -filter "Name -eq '$newUsername'"){
-                continue;
-            } else{
-                new-aduser -name $newUsername -path "OU=test,DC=colors,DC=labb";
-            }
-        }
-    } else{
-        new-aduser -name $username -path "OU=test,DC=colors,DC=labb";
-    }
+    new-aduser -name $username -path "OU=LightSalmon,DC=colors,DC=labb";
 }
