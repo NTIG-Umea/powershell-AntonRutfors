ab098220d7b7d1d838381601f46347bc72e62e6a
Anton Rutfors <anton.rutfors@elev.ga.ntig.se>
2023-12-14 12:44:57 -0800

    added log to create users server script

--- create-users-server.ps1
+++ create-users-server.ps1
@@ -1,6 +1,7 @@
 ﻿$ErrorActionPreference = 'silentlycontinue'
 
 $users = Import-Csv .\users2.csv;
+$log;
 
 foreach($user in $users){
     $username = $user.firstname + "." + $user.lastname;
@@ -8,9 +9,14 @@ foreach($user in $users){
     $counter = 2;
 
     while(get-aduser -identity $newuser) {
+        $log += "user $newuser exists`n";
         $newuser = $username + $counter;
         $counter++;
     }
 
+    $log += " - creating user: $newuser`n";
+
     new-aduser -name $newuser -path "OU=LightSalmon,DC=colors,DC=labb";
 }
+
+$log >> log.txt
